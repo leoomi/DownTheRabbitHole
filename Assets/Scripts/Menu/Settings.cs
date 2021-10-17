f9be5e782c5c905a5c735fd795326ba4dfd08b95
RedNyanCat <awsomenessAlex20@gmail.com>
2021-10-18 00:15:02 +0800

    Update of Settings
    + methods for applying audio and visual settings
    % Mixers
    % Menu UI

--- Assets/Scripts/Menu/Settings.cs
+++ Assets/Scripts/Menu/Settings.cs
@@ -1,10 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.Audio;
 
 public class Settings : MonoBehaviour
 {
 
+    [SerializeField]
+    UserSettings userSettings;
+    [SerializeField]
+    UnityEngine.UI.Slider volumeSFXSlider;
+    [SerializeField]
+    UnityEngine.UI.Slider volumeMusicSlider;
+    AudioMixer SFXMixer;
+    AudioMixer MusicMixer;
+    GameObject master;
+
+    // Should only be initialised once on seperate gameobject
+    // UI should only be initialised once on same gameobject as the settings.
     [System.Serializable]
     public struct UserSettings
     {
@@ -13,9 +28,7 @@ public class Settings : MonoBehaviour
         [Range(0,2)]
         public int vsync;
         public Resolution res;
-        [Range(0, 1)]
         public float volumeMusic;
-        [Range(0, 1)]
         public float volumeSFX;
         [Range(0, 4)]
         public int qualityLevel;
@@ -24,42 +37,59 @@ public class Settings : MonoBehaviour
 
         public UserSettings(int fr, int vs, Resolution r, float vm, float vsy, int ql, bool fs, AudioSpeakerMode asm)
         {
-            this.framerate = fr;
-            this.vsync = vs;
-            this.res = r;
-            this.volumeMusic = (1 > vm ? 1 : vm < 0 ? 0 : vm);
-            this.volumeSFX = (1 > vsy ? 1 : vsy < 0 ? 0 : vsy);
-            this.qualityLevel = ql;
-            this.fullscreen = fs;
-            this.audioMode = asm;
+            framerate = fr;
+            vsync = vs;
+            res = r;
+            volumeMusic = (1 > vm ? 1 : vm < 0 ? 0 : vm);
+            volumeSFX = (1 > vsy ? 1 : vsy < 0 ? 0 : vsy);
+            qualityLevel = ql;
+            fullscreen = fs;
+            audioMode = asm;
         }
 
     }
 
-    protected void loadJSON(UserSettings userSettings)
+    // load me daddy
+    protected void loadJSON()
     {
-        userSettings.framerate = 0;
-        userSettings.vsync = 0;
+        // actually load json at some point.
+
+        UserSettings userSettings = new UserSettings(
+            120,
+            0,
+            Screen.resolutions[0],
+            0.5f,
+            0.7f,
+            QualitySettings.GetQualityLevel(),
+            false,
+            AudioSpeakerMode.Stereo
+        );
     }
 
-    protected void applySettings(UserSettings userSettings)
+    protected void applySettings()
     {
+        // everything thats not audio settings
         Application.targetFrameRate = userSettings.framerate;
         QualitySettings.vSyncCount = userSettings.vsync;
         Screen.SetResolution(userSettings.res.width, userSettings.res.height, userSettings.fullscreen);
         QualitySettings.SetQualityLevel(userSettings.qualityLevel);
+    }
 
-        //figure out volume
+    public void audioSettings()
+    {
+        // audio settings should get their own page?
+        float volumeSFX = volumeSFXSlider.value;
+        float volumeMusic = volumeMusicSlider.value;
+        SFXMixer.SetFloat("SFX", volumeSFX);
+        MusicMixer.SetFloat("MUSIC", volumeMusic);
         AudioSettings.speakerMode = userSettings.audioMode;
-
-
     }
 
     // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this);
-        UserSettings userSettings = new UserSettings();
+        
     }
 
     // Update is called once per frame
