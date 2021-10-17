ebd5eeaab80389670da84c86f9af85e2b75cb6e7
RedNyanCat <awsomenessAlex20@gmail.com>
2021-10-17 21:01:21 +0800

    Revert "Minor changes to Ice"
    This reverts commit 9a137e04263eaf45b833581a28fde6a1da253f48.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,9 @@ public class PlayerController : MonoBehaviour
     private float slowSpeedPercent = 0.5f;
     // rigid-body stuff
     [SerializeField]
-    private float[] linearDrag = new float[3] { 16f, 4f, 1f };
+    private float[] linearDrag = new float[3] { 16f, 4f, 0.05f };
     [SerializeField]
-    private float[] angularDrag = new float[3] { 0.05f, 0.15f, 0.02f };
+    private float[] angularDrag = new float[3] { 0.05f, 0.15f, 0.01f };
     #endregion
 
     #region Publics
