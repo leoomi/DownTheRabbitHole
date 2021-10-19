468e3fe74ca281db2778232c353c4351fce4fa85
RedNyanCat <awsomenessAlex20@gmail.com>
2021-10-19 23:01:13 +0800

    Revert "Revert "Homing projectiles!""
    This reverts commit 8d41c2842463ea375d7ce7da6b2b5db3b7c7f587.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -6,9 +6,16 @@ public class Projectile : MonoBehaviour
 {
     public int maxBounces { get; set; }
     public float timeToLive { get; set; }
+    public float angleChangingSpeed { get; set; } = 400;
     private Rigidbody2D myRigibody;
     private int bounces = 0;
-
+    public Transform target = null;
+    [SerializeField]
+    private float waitBeforeLockon = 1.5f;
+    private float shotVelocity = 1f;
+    private bool bounce = false;
+    private float waittime;
+    
     void Awake()
     {
         myRigibody = GetComponent<Rigidbody2D>();
@@ -19,30 +26,57 @@ public class Projectile : MonoBehaviour
         StartCoroutine(DestroyCoroutine());
     }
 
-    public void SetVelocity(Vector2 velocity)
+    // factory design method babyyy
+    public static GameObject CreateProjectile(GameObject projectilePrefab, Vector2 velocity, Transform shotOrigin, Quaternion q, float shotVelocity=1f, float waittime = 0.1f)
     {
-        myRigibody.velocity = velocity;
+        var projectileInstance = Instantiate(projectilePrefab.gameObject, shotOrigin.position, Quaternion.identity);
+
+        Projectile Instance = projectileInstance.AddComponent<Projectile>();
+        Instance.myRigibody.velocity = velocity * shotVelocity;
+        Instance.shotVelocity = shotVelocity;
+        Instance.waittime = waittime;
+        Instance.gameObject.transform.SetPositionAndRotation(Instance.gameObject.transform.position, shotOrigin.rotation);
+        //Instance.myRigibody
+
+        return projectileInstance;
+    }
+
+    public void SetTarget(Transform t)
+    {
+        target = t;
     }
 
     public IEnumerator DestroyCoroutine()
     {
+        // maybe add animation and sfx - Red
         yield return new WaitForSeconds(timeToLive);
         Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        //print(target);
+        if (target) {
+            Vector2 direction = (Vector2) target.position - myRigibody.position;
+            direction.Normalize();
+            float rot = Vector3.Cross(direction, transform.up).z;
+            //print(rot);
+            // wait before changing the velocity
+            //new WaitForSeconds(waitBeforeLockon);
+            // StartCoroutine(wait());
+            if (bounce is false || (target != null && bounce is true)) myRigibody.angularVelocity = transform.rotation.z + -angleChangingSpeed * rot;
+            // print(-angleChangingSpeed * rot);
+            if (bounce is false || (target != null && bounce is true)) myRigibody.velocity = transform.up * shotVelocity;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         // Zero is infinite
-        if (maxBounces == 0)
-        {
-            return;
-        }
+        if (maxBounces == 0) return;
 
         var hasProjectileDestroyer = other.gameObject.TryGetComponent<ProjectileDestroyer>(out var _);
-        if (hasProjectileDestroyer)
-        {
-            return;
-        }
+        if (hasProjectileDestroyer) return;
 
         bounces += 1;
         if (bounces >= maxBounces)
@@ -50,4 +84,33 @@ public class Projectile : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    public IEnumerator waitToSetTarget(Transform t, float seconds = 0.5f) 
+    { 
+        yield return new WaitForSeconds(seconds); 
+        target = t;
+        print(t);
+    }
+
+    public IEnumerator forgetTarget(float seconds=0.1f)
+    {
+        Transform actualTarget = null;
+        bounce = true;
+        if (target) { 
+            actualTarget = target;
+            target = null;
+        }
+
+        yield return new WaitForSeconds(seconds);
+
+        if (actualTarget) { target = actualTarget; }
+        bounce = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Ice" || other.gameObject.tag == "Mud") return;
+        StartCoroutine(forgetTarget(waittime));
+    }
+
 }
