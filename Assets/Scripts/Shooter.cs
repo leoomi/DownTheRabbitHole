948c7c84f8762ceefeb6e8d12d02e3ea49d51f75
Leo Yuuki Omori Omi <leoyuuki@gmail.com>
2021-10-20 21:48:31 -0300

    Adjust light add fireflies and new stage

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -16,9 +16,13 @@ public class Shooter : MonoBehaviour
     [SerializeField]
     private float initialDelay = 0f;
     [SerializeField]
+    private bool shootImmediatelyAfterDelay = false;
+    [SerializeField]
     private float projectileTimeToLive = 3f;
     [SerializeField]
     private int projectileMaxBounces = 0;
+    [SerializeField]
+    private float projectileLightRadius = 0f;
     [SerializeField]
     private bool heatseeking = false;
     [SerializeField]
@@ -39,29 +43,41 @@ public class Shooter : MonoBehaviour
     private IEnumerator ShootCoroutine()
     {
         yield return new WaitForSeconds(initialDelay);
+        if (shootImmediatelyAfterDelay)
+        {
+            Shoot();
+        }
 
         if (heatseeking is true && target == null) target = GameObject.FindGameObjectWithTag("Player");
 
         while (true)
         {
             yield return new WaitForSeconds(delayBetweenShots);
-            for (int i = 0; i < shotOrigin.Length; i++)
-            {
-                GameObject projectileInstance = Projectile.CreateProjectile(projectilePrefab, -shotOrigin[i].transform.up, shotOrigin[i], Quaternion.identity, shotVelocity, projectileForgetTime);
-                var projectile = projectileInstance.GetComponent<Projectile>();
-                //projectileInstance.AddComponent(projectileScript);
-
-                projectile.timeToLive = projectileTimeToLive;
-                projectile.maxBounces = projectileMaxBounces;
-
-                //Debug.Log(transform.up);
-
-                StartCoroutine(projectile.waitToSetTarget(heatseeking is true ? target != null ? target.transform : null : null));
-                //projectile.SetTarget(heatseeking is true ? target != null ? target.transform : null : null);
-                //projectile.forgetTarget(2f);
-                //projectile.SetInitialVelocity(-shotOrigin[i].transform.up * shotVelocity);
-            }
+            Shoot();
         }
     }
 
+    private void Shoot()
+    {
+        for (int i = 0; i < shotOrigin.Length; i++)
+        {
+            GameObject projectileInstance = Projectile.CreateProjectile(projectilePrefab, -shotOrigin[i].transform.up, shotOrigin[i], Quaternion.identity, shotVelocity, projectileForgetTime);
+            var projectile = projectileInstance.GetComponent<Projectile>();
+            //projectileInstance.AddComponent(projectileScript);
+
+            projectile.timeToLive = projectileTimeToLive;
+            projectile.maxBounces = projectileMaxBounces;
+            if (projectileLightRadius != 0f)
+            {
+                projectile.SetLightRadius(projectileLightRadius);
+            }
+
+            //Debug.Log(transform.up);
+
+            StartCoroutine(projectile.waitToSetTarget(heatseeking is true ? target != null ? target.transform : null : null));
+            //projectile.SetTarget(heatseeking is true ? target != null ? target.transform : null : null);
+            //projectile.forgetTarget(2f);
+            //projectile.SetInitialVelocity(-shotOrigin[i].transform.up * shotVelocity);
+        }
+    }
 }
